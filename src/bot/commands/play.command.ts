0b163b1373b92d13a515082a785bf2bc1627b44a
Fesyse <maksimlol.mail.com@mail.ru>
2024-06-28 23:20:29 +0800

    feat: add buttons to message embed play command

--- src/bot/commands/play.command.ts
+++ src/bot/commands/play.command.ts
@@ -6,12 +6,21 @@ import {
 	InteractionEvent,
 	MessageEvent
 } from "@discord-nestjs/core"
-import { ClientEvents, EmbedBuilder, Message } from "discord.js"
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ClientEvents,
+	EmbedBuilder,
+	Message,
+	User
+} from "discord.js"
 
 import { PlayDto } from "../dto/play.dto"
 import { joinVoiceChannel } from "@discordjs/voice"
 import { YoutubeService } from "@/youtube/youtube.service"
 import { imageSize } from "@/utils"
+import { Music } from "ytubes/dist/types/data"
 
 @Command({
 	name: "play",
@@ -19,6 +28,65 @@ import { imageSize } from "@/utils"
 })
 export class PlayCommand {
 	constructor(private youtubeService: YoutubeService) {}
+
+	getPlayCommandEmbed(message: Message, selectedMusic: Music) {
+		let user: User
+		if ("user" in message) user = message.user as typeof message.author
+		else user = message.author
+
+		const embed = new EmbedBuilder()
+			.setColor("#fff0db")
+			.setTitle(`:musical_note:  Playing song \`${selectedMusic.title}\``)
+			.setAuthor({
+				name: user.displayName,
+				iconURL: user.avatarURL() ?? "/img/user.svg",
+				url: "https://discord.js.org"
+			})
+			.setThumbnail(selectedMusic.thumbnail)
+			.addFields(
+				{
+					name: ":mens: Requested by",
+					value: user.toString(),
+					inline: true
+				},
+				{
+					name: ":clock1: Music duration",
+					value: `\`${selectedMusic.duration}\``,
+					inline: true
+				},
+				{
+					name: ":copyright: Song author",
+					value: `\`${selectedMusic.artist}\``,
+					inline: true
+				}
+			)
+			.setTimestamp()
+			.setFooter({
+				text: "Fythm",
+				iconURL: "https://i.ibb.co/W6c32cx/fythm-logo.png"
+			})
+		return embed
+	}
+	getPlayCommandActionButtonRows() {
+		const firstButtonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+			new ButtonBuilder()
+				.setCustomId("volume-down")
+				.setLabel("- Vol")
+				.setStyle(ButtonStyle.Secondary)
+				.setEmoji("🔉"),
+			new ButtonBuilder()
+				.setCustomId("volume-mute")
+				.setLabel("Mute")
+				.setStyle(ButtonStyle.Secondary)
+				.setEmoji("🔇"),
+			new ButtonBuilder()
+				.setCustomId("volume-up")
+				.setLabel("+ Vol")
+				.setStyle(ButtonStyle.Secondary)
+				.setEmoji("🔊")
+		)
+		return [firstButtonRow]
+	}
 
 	joinVoiceChannel(message: Message) {
 		const connection = joinVoiceChannel({
@@ -51,43 +119,11 @@ export class PlayCommand {
 		const selectedMusic = music[0]
 		if (!music.length) return `No music found with given prompt.`
 
-		// @ts-ignore
-		const user = message.author ?? (message.user as typeof message.author)
+		const embed = this.getPlayCommandEmbed(message, selectedMusic)
 
-		const embed = new EmbedBuilder()
-			.setColor(0x0099ff)
-			.setTitle(`:musical_note:  Playing song \`${selectedMusic.title}\``)
-			.setAuthor({
-				name: user.displayName,
-				iconURL: user.avatarURL() ?? "/img/user.svg",
-				url: "https://discord.js.org"
-			})
-			.setThumbnail(selectedMusic.thumbnail)
-			.addFields(
-				{
-					name: ":mens: Requested by",
-					value: user.toString(),
-					inline: true
-				},
-				{
-					name: ":clock1: Music duration",
-					value: `\`${selectedMusic.duration}\``,
-					inline: true
-				},
-				{
-					name: ":copyright: Song author",
-					value: `\`${selectedMusic.artist}\``,
-					inline: true
-				}
-			)
-			.setTimestamp()
-			.setFooter({
-				text: "Fythm",
-				iconURL: "https://i.ibb.co/W6c32cx/fythm-logo.png"
-			})
-
-		message.reply({
-			embeds: [embed]
+		await message.reply({
+			embeds: [embed],
+			components: [...this.getPlayCommandActionButtonRows()]
 		})
 	}
 }
